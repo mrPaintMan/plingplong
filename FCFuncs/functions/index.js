3e7259378539980985b9a75e9611ba76c5407dec
Filip Palmqvist <fille.palmqvist@icloud.com>
2023-02-08 22:48:53 +0100

    corrected FCM function

--- FCFuncs/functions/index.js
+++ FCFuncs/functions/index.js
@@ -2,31 +2,57 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-//http request method
+// http request method
 exports.sendHttpPushNotification = functions.https.onRequest((req, res) => {
-  const user = req.body.user; //get params like this
-  const FCMToken = admin.database().ref(`/FCMTokens/${user}`).once("value");
+  const user = req.body.user; // get params like this
+  const msg = req.body.msg ? req.body.msg : "Mälski vill ha uppmärksamhet!!";
+  const title = req.body.title ? req.body.title : "Uppmärksamhets Alert";
 
   const payload = {
-    token: FCMToken,
     notification: {
-      title: "cloud function demo",
-      body: "Mälski vill ha uppmärksamhet!!",
+      title: title,
+      body: msg,
     },
-    data: {
-      body: "Mälski vill ha uppmärksamhet!!",
+    android: {
+      notification: {
+        sound: "default",
+      },
+    },
+    apns: {
+      payload: {
+        aps: {
+          sound: "default",
+        },
+      },
     },
   };
+  return admin
+    .database()
+    .ref(`/FCMTokens/${user}`)
+    .once("value")
+    .then(
+      (snapshot) => {
+        payload.token = snapshot.val();
 
-  admin
-    .messaging()
-    .send(payload)
-    .then((response) => {
-      // Response is a message ID string.
-      console.log("Successfully sent message:", response);
-      return { success: true };
-    })
-    .catch((error) => {
-      return { error: error.code };
-    });
+        return admin
+          .messaging()
+          .send(payload)
+          .then((response) => {
+            // Response is a message ID string.
+            res.status = 200;
+            res.json({ result: "Message sent" });
+            return { success: true };
+          })
+          .catch((error) => {
+            res.status = 500;
+            res.json({ result: "Message not sent", error: error });
+            return { error: error.code };
+          });
+      },
+      (error) => {
+        res.status = 400;
+        res.json({ result: "User not found" });
+        return { error: error.code };
+      }
+    );
 });
