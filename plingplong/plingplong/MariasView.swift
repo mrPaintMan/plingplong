f7f5d870efb33901ccce71776b8ad33096ed56c5
Filip Palmqvist <fille.palmqvist@icloud.com>
2023-02-09 00:08:46 +0100

    Added notifier to marias view

--- plingplong/plingplong/MariasView.swift
+++ plingplong/plingplong/MariasView.swift
@@ -8,26 +8,73 @@
 import SwiftUI
 
 struct MariasView: View {
-    var body: some View {
-        Button {
-            print("pushed!")
-        } label: {
-            Text("Pling Plong Filip")
-                .foregroundColor(.white)
-                .padding(100)
-                .background {
-                    ZStack{
-                        Image("MariasDefaultImage")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .clipShape(Circle())
-                        Circle()
-                            .fill(LinearGradient(gradient: Gradient(colors: [.blue, .blue.opacity(0.7)]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                            .opacity(0.7)
-                    }
-                }
+    @State var hasSent = false
+    
+    private struct Payload: Codable {
+        var user: String
+    }
+    
+    private func sendFilipMessage() async {
+        guard let payload = try? JSONEncoder().encode(Payload(user: "Filip")) else {
+            print("Failed to encode payload")
+            return
         }
-
+        
+        let url = URL(string: "https://us-central1-plingplong-19030.cloudfunctions.net/sendHttpPushNotification")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (_, res) = try await URLSession.shared.upload(for: request, from: payload)
+            hasSent = (res as? HTTPURLResponse)?.statusCode == 200
+            
+        } catch {
+            print("Failed to send message")
+        }
+        
+    }
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Spacer()
+            Text("Filip är underrättad.")
+                .foregroundColor(.red)
+                .opacity(hasSent ? 1 : 0)
+            
+            Spacer()
+            Button {
+                hasSent = false
+                Task {
+                    await sendFilipMessage()
+                }
+                print("pushed!")
+            } label: {
+                Text("Pling Plong Filip")
+                    .font(Font.title)
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(70)
+                    .background {
+                        ZStack{
+                            Image("MariasDefaultImage")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .clipShape(Circle())
+                            Circle()
+                                .strokeBorder(Color.white, lineWidth: 5)
+                                .background(content: {
+                                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.blue, .blue.opacity(0.7)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                                        .opacity(0.7)
+                                })
+                                .shadow(radius: 10)
+                        }
+                    }
+            }
+            Spacer()
+            Spacer()
+        }
     }
 }
 
